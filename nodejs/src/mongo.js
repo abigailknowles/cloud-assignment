9e1bf3e78308eeca72da030cb49b16dad5775c84
abigailknowles <abigailknowlesx@yahoo.co.uk>
2022-03-07 11:21:38 +0000

    Intial commit - create file structure for node js and src, Also adapted the mongo.js file to suit the NotFlix DB requirments. I also created the database and populated it with some sample data.

---
+++ nodejs/src/mongo.js
@@ -0,0 +1,80 @@
+
+//Object data modelling library for mongo
+const mongoose = require('mongoose');
+
+//Mongo db client library
+//const MongoClient  = require('mongodb');
+
+//Express web service library
+const express = require('express')
+
+//used to parse the server response from json to object.
+const bodyParser = require('body-parser');
+
+//instance of express and port to use for inbound connections.
+const app = express()
+const port = 3000
+
+//connection string listing the mongo servers. This is an alternative to using a load balancer. THIS SHOULD BE DISCUSSED IN YOUR ASSIGNMENT.
+const connectionString = 'mongodb://localmongo1:27017,localmongo2:27017,localmongo3:27017/notFLIX_DB?replicaSet=rs0';
+
+setInterval(function () {
+
+  console.log(`Intervals are used to fire a function for the lifetime of an application.`);
+
+}, 3000);
+
+//tell express to use the body parser. Note - This function was built into express but then moved to a seperate package.
+app.use(bodyParser.json());
+
+//connect to the cluster
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var Schema = mongoose.Schema;
+
+var analyticsSchema = new Schema({
+  _id: Number,
+  username: String,
+  title_id: Number,
+  user_action: String,
+  date_and_time: String,
+  point_of_interaction: String,
+  type_of_interaction: String
+});
+
+var analyticsModel = mongoose.model('Analytics', analyticsSchema, 'analytics');
+
+
+
+app.get('/', (req, res) => {
+  analyticsModel.find({}, 'item price quantity lastName', (err, analytics) => {
+    if (err) return handleError(err);
+    res.send(JSON.stringify(analytics))
+  })
+})
+
+app.post('/', (req, res) => {
+  var awesome_instance = new SomeModel(req.body);
+  awesome_instance.save(function (err) {
+    if (err) res.send('Error');
+    res.send(JSON.stringify(req.body))
+  });
+})
+
+app.put('/', (req, res) => {
+  res.send('Got a PUT request at /')
+})
+
+app.delete('/', (req, res) => {
+  res.send('Got a DELETE request at /')
+})
+
+//bind the express web service to the port specified
+app.listen(port, () => {
+  console.log(`Express Application listening at port ` + port)
+})
+
