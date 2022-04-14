f91e0211f2ed0a317632e1d03df1ed12bb275b7c
abigailknowles <abigailknowlesx@yahoo.co.uk>
2022-04-14 18:54:41 +0100

    fixed broken publisher

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -33,10 +33,11 @@ var nodeId = Math.floor(Math.random() * (100 - 1 + 1) + 1);
 var seconds = new Date().getTime() / 1000;
 
 //create a list of details about the nodes
-var nodes = { nodeId: nodeId, hostname: hostname, isAlive: isAlive, lastSeenAlive: seconds };
+var nodes = { id: nodeId, hostname: hostname, isAlive: isAlive, lastSeenAlive: seconds };
 var messageList = [];
 messageList.push(nodes);
 
+//publisher
 setInterval(function () {
   // connect to haproxy
   amqp.connect('amqp://test:test@cloud-assignment_haproxy_1', function (error0, connection) {
@@ -50,13 +51,13 @@ setInterval(function () {
       if (error1) {
         throw error1;
       }
+
       exchange = 'NODE ALIVE';
       seconds = new Date().getTime() / 1000;
       isAlive = true;
 
-      msg = `\n\r----------------------------------------------------------------------------------------------------------------------\n\r\n\r`;
-      msg += `{Node ID: ${nodeId}, hostname: ${hostname} ${isAlive ? "is alive" : "is dead"} last seen ${seconds} seconds ago}\n\r\n\r`;
-      msg += `----------------------------------------------------------------------------------------------------------------------\n\r`;
+      msg = `{"id": ${nodeId}, "hostname": ${hostname}, "isAlive": ${isAlive}, "lastSeenAlive": ${seconds}}`;
+      msg = JSON.stringify(JSON.parse(msg));
 
       channel.assertExchange(exchange, 'fanout', {
         durable: false
@@ -70,6 +71,7 @@ setInterval(function () {
     }, 500);
   });
 }, 3000);
+
 
 
 //bind the express web service to the port specified
@@ -128,42 +130,6 @@ amqp.connect('amqp://test:test@cloud-assignment_haproxy_1', function (error0, co
     });
   });
 });
-
-
-//publisher
-setInterval(function () {
-  // connect to haproxy
-  amqp.connect('amqp://test:test@cloud-assignment_haproxy_1', function (error0, connection) {
-
-    //if connection failed throw error
-    if (error0) {
-      throw error0;
-    }
-    //create a channel if connected and send hello world to the logs Q
-    connection.createChannel(function (error1, channel) {
-      if (error1) {
-        throw error1;
-      }
-
-      exchange = 'NODE ALIVE';
-      seconds = new Date().getTime() / 1000;
-      isAlive = true;
-
-      msg = `{"id": ${nodeId}, "hostname": ${hostname}, "isAlive": ${isAlive} "lastSeenAlive": ${seconds}}`;
-      msg = JSON.stringify(JSON.parse(msg));
-
-      channel.assertExchange(exchange, 'fanout', {
-        durable: false
-      });
-      channel.publish(exchange, '', Buffer.from(msg));
-      console.log("[x] Sent %s", msg);
-    });
-    //in 1/2 a second force close the connection
-    setTimeout(function () {
-      connection.close();
-    }, 500);
-  });
-}, 3000);
 
 //tell express to use the body parser. Note - This function was built into express but then moved to a seperate package.
 app.use(bodyParser.json());
